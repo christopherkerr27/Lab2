6d2e94c0120d1851bf91ef12a14c1233b4e4f970
christopherkerr27 <56808327+christopherkerr27@users.noreply.github.com>
2020-04-20 20:27:10 -0400

    Add files via upload

---
+++ Lab2/Lab2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Lab2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string response = "";
+            do
+            {
+                Console.WriteLine("Enter Length:");
+                var num = Console.ReadLine();
+                float length = float.Parse(num);
+                Console.Write("Enter Width:");
+                var num2 = Console.ReadLine();
+                float width = float.Parse(num2);
+                var area = length * width;
+                Console.WriteLine("Area: " + area);
+                var perimeter = length + length + width + width;
+                Console.WriteLine("Perimeter: " + perimeter);
+                Console.WriteLine("Would you like to continue? (y/n)");
+                response = Console.ReadLine();
+                response = response.ToUpper();
+            }
+            while (response == "Y");
+            Console.WriteLine("Goodbye!");
+        }
+    }
+}
